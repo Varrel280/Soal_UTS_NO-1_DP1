3adddbc56e966c6699be36a7340f24e183f2cc01
Varrel280 <varrel1707hutabarat@gmail.com>
2024-10-31 21:56:58 +0700

    Update Soal3.pas

--- Soal3.pas
+++ Soal3.pas
@@ -1,1 +1,26 @@
-//ketik kode jawaban noo 3 kamu disini
+program soalutsno3; //nama program 
+uses crt; 
+var
+    i,n,t:integer; //variabel
+begin //mulai
+    clrscr; //untuk clearscreen dan membuang code yang tidak diperlukan
+    Write('n = '); readln(n); //input nilai n
+    if (n mod 2 = 1) then //jika n di mod 2 = 1, berarti ganjil maka
+    begin //masuk ke program ini 
+        for i:= 1 to n do //loop untuk setiap baris
+        begin //memulai
+            for t:= 1 to n do //loop untuk setiap kolom
+                begin //mulai
+                    if (i = t) or (i + t = n + 1) then //memastikan sama atau tidak sesuai kondisi
+                    write ('*') //cetak bintang jika iya
+                    else //kalau nggak
+                    write (' ') //maka cetak spasi
+                end; //berakhir sesuai kolom
+            writeln; //enter kebawah jika sudah siap di baris pertama dan setersunya
+        end //berakhir
+    end //program if berakhir
+    else //jika tidak
+    begin //mulai
+        Writeln('angka tidak valid, harus angka ganjil'); //jika genap maka akan ada tampilan error
+    end; //berakhir
+end. //selesai
